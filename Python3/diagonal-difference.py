c683b9cee61c16cbd90d57ec7021c31c57c99035
Cole Milne <colemilne54@gmail.com>
2023-01-26 11:52:12 -0800

    Diagonal Difference

---
+++ Python3/diagonal-difference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    sumLR = 0
+    sumRL = 0
+
+    for i in range(n):
+        sumLR += arr[i][i]
+        sumRL += arr[i][n-(i+1)]
+        print(sumLR)
+        print(sumRL)
+
+    return abs(sumLR-sumRL)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
